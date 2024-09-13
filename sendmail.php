31714926aa4ed4443455ffe0dd35f2485d10b1c6
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-09-13 11:26:53 +0545

    mail

--- sendmail.php
+++ sendmail.php
@@ -5,7 +5,6 @@ $subject = "Test mail";
 $message = "Hello! This is a simple email message.";
 $from = "sudip.parajulee@gmail.com";
 $headers = "From:" . $from;
-
 mail($to, $subject, $message, $headers);
 echo "Mail Sent.";
 ?>
