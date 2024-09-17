4072d413dde93b61891670d2396384c4dcea11fc
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-09-17 10:57:36 +0545

    MYSQL Select

---
+++ mysql/select.php
@@ -0,0 +1,35 @@
+<?php 
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "lict";
+
+//connect to database
+$conn = new mysqli($server, $user, $pass, $db);
+
+//check connection
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//query to select data
+$qry = "SELECT name,phone,email,address FROM students WHERE address='Chitwan'";
+
+//execute query
+$result = $conn->query($qry);
+
+if($result->num_rows > 0){
+    //output data of each row
+    while($row = $result->fetch_assoc()){
+        // echo "ID: " . $row['id']."<br>";
+        echo "Name: " . $row['name']."<br>";
+        echo "Phone: " . $row['phone']."<br>";
+        echo "Email: " . $row['email']."<br>";
+        echo "Address: " . $row['address']."<br><br>";
+    }
+}else{
+    echo "0 results";
+}
+
+$conn->close();
+?>
